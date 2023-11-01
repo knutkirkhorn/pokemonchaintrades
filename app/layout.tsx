6d34b1579184e444f0a8d62fa29f792d33d1d905
Knut Kirkhorn <knutkirk@hotmail.com>
2023-11-01 17:32:15 +0100

    Add Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import type {Metadata} from 'next';
 import {Inter} from 'next/font/google';
 import React from 'react';
+import {Analytics} from '@vercel/analytics/react';
 
 const inter = Inter({subsets: ['latin']});
 
@@ -22,6 +23,7 @@ export default function RootLayout({
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 					{children}
 				</ThemeProvider>
+				<Analytics />
 			</body>
 		</html>
 	);

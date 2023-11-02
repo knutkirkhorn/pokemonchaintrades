178673e02eec9a049c439419537042817f82da1b
Knut Kirkhorn <knutkirk@hotmail.com>
2023-11-03 00:22:07 +0100

    Minor cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,14 @@ export default function Home() {
 
 	const onAddStep = () => {
 		const lastPokemon = pokemonTrades.at(-1)?.secondPokemon ?? {name: '', language: ''};
-		const newPokemonTrades = [...pokemonTrades, {id: crypto.randomUUID(), firstPokemon: lastPokemon, secondPokemon: {name: '', language: '' as Pokemon['language']}}];
+		const newPokemonTrades = [...pokemonTrades, {
+			id: crypto.randomUUID(),
+			firstPokemon: lastPokemon,
+			secondPokemon: {
+				name: '',
+				language: '' as Pokemon['language'],
+			},
+		}];
 		setPokemonTrades(newPokemonTrades);
 		localStorage.setItem('pokemonTrades', JSON.stringify(newPokemonTrades));
 	};
